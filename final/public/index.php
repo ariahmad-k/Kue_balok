dd66a51d475f38bc0c77412d25752c8b07b258b6
AK Kurtubi <ariahmadkurtubi@gmail.com>
2025-05-15 00:45:00 +0700

    first commit

---
+++ final/public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<bo>
+    <h1>ini adalah h1</h1>
+    lsdfjglsjfdlskdfjglsdfjgsldkfjglskdfg sdklfgsdlfgjs
+    <table>
+        <>
+            <td>sdgsdgdsf</td>
+            <td>asdfasdsfasdfas</td>
+        </tr>
+    </table>
+
+    asdfasdsfasdfas
+    fdgjdlfsdflgsdfg
+</body>
+</html>
